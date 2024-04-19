d29bb02f6dd13aee220da575172d1d3fc8937143
Jorge Parreira <44405682+JorgeParreira@users.noreply.github.com>
2024-04-19 12:58:18 +0100

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/cursoudemy/course/config/TestConfig.java
@@ -0,0 +1,34 @@
+package com.cursoudemy.course.config;
+
+import com.cursoudemy.course.entities.User;
+import com.cursoudemy.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        // Grava os users numa lista sem ter de criar uma nova arrayList e adicionar os users um a um
+        userRepository.saveAll(Arrays.asList(u1, u2));
+
+
+
+    }
+}
