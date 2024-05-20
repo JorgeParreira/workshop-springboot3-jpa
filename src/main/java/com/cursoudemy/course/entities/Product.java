ce0bfe770e70617846920baede7fc38d8388a89d
Jorge Parreira <44405682+JorgeParreira@users.noreply.github.com>
2024-05-20 17:44:13 +0100

    Product-OrderItem one-to-many association

--- src/main/java/com/cursoudemy/course/entities/Product.java
+++ src/main/java/com/cursoudemy/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.cursoudemy.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
@@ -16,11 +17,15 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
+
+    //Estamos a usar o Set invés do List para informar o JPA que não estamos a admitir repetições
     @ManyToMany
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {}
 
@@ -75,6 +80,16 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x: items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
