c85df70df20e9ae3e18b57dc63c55b7fae74e8d8
Jorge Parreira <44405682+JorgeParreira@users.noreply.github.com>
2024-05-22 17:40:28 +0100

    User insert

--- src/main/java/com/cursoudemy/course/resources/UserResource.java
+++ src/main/java/com/cursoudemy/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.cursoudemy.course.entities.User;
 import com.cursoudemy.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +26,15 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Optional<User>> findyById(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUser(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
--- src/main/java/com/cursoudemy/course/services/UserService.java
+++ src/main/java/com/cursoudemy/course/services/UserService.java
@@ -20,4 +20,8 @@ public class UserService {
         return userRepository.findById(id);
     }
 
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
 }
